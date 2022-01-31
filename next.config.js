af536cfffb8098bc9c55785969c941507ab8e7f3
unknown <rohithpenubadi@gmail.com>
2022-01-31 16:48:57 +0530

    final commit

--- next.config.js
+++ next.config.js
@@ -2,5 +2,10 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["assets.stickpng.com"],
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
